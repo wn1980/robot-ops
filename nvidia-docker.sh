14af04cb9c482c9ce67df1e68466f0940a1fbbce
wn1980 <waipotn@hotmail.com>
2021-11-10 19:10:29 +0700

    add test nvidia-docker

--- nvidia-docker.sh
+++ nvidia-docker.sh
@@ -9,3 +9,6 @@ curl -s -L https://nvidia.github.io/nvidia-docker/$distribution/nvidia-docker.li
 
 sudo apt-get update && sudo apt-get install -y nvidia-docker2
 sudo systemctl restart docker
+
+#test
+sudo docker run --rm --gpus all nvidia/cuda:11.0-base nvidia-smi
