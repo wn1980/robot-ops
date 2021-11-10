5cac224f51f729f0473136537d569d022c3a9177
wn1980 <waipotn@hotmail.com>
2021-11-10 09:36:42 +0700

    add dailout

--- docker.sh
+++ docker.sh
@@ -6,5 +6,6 @@ sudo apt update && sudo apt install -y \
     docker.io 
 
 sudo usermod -aG docker $USER
+sudo usermod -aG dialout $USER
 
 echo -e "\n\n====================\nInstallation done!\nPlease reboot for completion.\n===================="
