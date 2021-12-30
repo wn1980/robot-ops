71259a8c40215bfb01c7c121a8fa7c06814b0e4c
wn1980 <waipotn@hotmail.com>
2021-12-31 06:39:55 +0700

    add install code-server.sh

--- ros1/image/app/install/turtlebot2.sh
+++ ros1/image/app/install/turtlebot2.sh
@@ -24,11 +24,11 @@ git clone https://github.com/turtlebot/turtlebot_viz.git
 git clone --single-branch --branch melodic https://github.com/yujinrobot/kobuki.git
 mv kobuki/kobuki_description kobuki/kobuki_node \
   kobuki/kobuki_keyop kobuki/kobuki_safety_controller \
-  kobuki/kobuki_bumper2pc ./
+  kobuki/kobuki_bumper2pc kobuki/kobuki_auto_docking ./
 rm -rf kobuki
 
 git clone --single-branch --branch melodic https://github.com/yujinrobot/kobuki_desktop.git
-mv kobuki_desktop/kobuki_gazebo_plugins ./
+mv kobuki_desktop/kobuki_gazebo_plugins kobuki_desktop/kobuki_dashboard ./
 rm -rf kobuki_desktop
 
 git clone https://github.com/yujinrobot/yujin_ocs.git
@@ -49,6 +49,7 @@ sudo apt-get install  -y \
   ros-${ROS_DISTRO}-robot-state-publisher \
   ros-${ROS_DISTRO}-diagnostics \
   ros-${ROS_DISTRO}-openni2-launch \
+  ros-${ROS_DISTRO}-rqt-robot-dashboard \
   libsensors4-dev
 
 # make and install
