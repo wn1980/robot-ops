71897d8b902c64e05345b0cfb7794e3c8b953a82
wn1980 <waipotn@hotmail.com>
2022-01-10 11:38:23 +0700

    move bootstrap to App.js

--- web-app/image/ros-web-control/src/App.js
+++ web-app/image/ros-web-control/src/App.js
@@ -1,3 +1,6 @@
+import "bootstrap/dist/css/bootstrap.min.css";
+import "bootstrap/dist/js/bootstrap.min.js";
+
 import Header from "./components/Header";
 import Footer from "./components/Footer";
 
--- web-app/image/ros-web-control/src/index.js
+++ web-app/image/ros-web-control/src/index.js
@@ -4,9 +4,6 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
-import "bootstrap/dist/css/bootstrap.min.css";
-import "bootstrap/dist/js/bootstrap.min.js";
-
 ReactDOM.render(
   <React.StrictMode>
     <App />
