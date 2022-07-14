994f474967cbd38663cb8befbfd44491bb1a165f
wn1980 <waipotn@hotmail.com>
2022-07-14 07:54:48 +0700

    update

--- rpi-swap.sh
+++ rpi-swap.sh
@@ -2,12 +2,23 @@
 
 set -e
 
-sudo dphys-swapfile swapoff 
+#if [ -z "$1" ]
+#  then
+#    echo "Please provide SWAPSIZE in MB, e.g., ./rpi-swap.sh 1024/2048/4096"
+#    exit 0
+#else
+#    SWAPSIZE=$1
+#fi
+
+#sudo dphys-swapfile swapoff 
 
 #sudo nano /etc/dphys-swapfile 
 
-sudo dphys-swapfile setup
+#sudo dphys-swapfile setup
 
-sudo dphys-swapfile swapon
+#sudo dphys-swapfile swapon
 
-sudo reboot
+#sudo reboot
+
+sudo sed -i 's/^CONF_SWAPSIZE=[0-9]*$/CONF_SWAPSIZE=2048/' /etc/dphys-swapfile
+sudo /etc/init.d/dphys-swapfile restart
