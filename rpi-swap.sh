26fdb633fd8a2908999137e2edea0e0054f204c1
wn1980 <waipotn@hotmail.com>
2022-07-15 20:26:01 +0700

    update

--- rpi-swap.sh
+++ rpi-swap.sh
@@ -2,13 +2,18 @@
 
 set -e
 
-#if [ -z "$1" ]
-#  then
-#    echo "Please provide SWAPSIZE in MB, e.g., ./rpi-swap.sh 1024/2048/4096"
-#    exit 0
-#else
-#    SWAPSIZE=$1
-#fi
+if [ -z "$1" ]
+  then
+    echo "Please provide SWAPSIZE in MB, e.g., ./rpi-swap.sh 1024/2048/4096"
+    exit 0
+else
+    SWAPSIZE=$1
+fi
+
+if ! [[ $SWAPSIZE =~ ^[0-9]+$ ]] ; then
+    echo "SWAPSIZE must be a number!"
+    exit 0
+fi
 
 #sudo dphys-swapfile swapoff 
 
@@ -20,5 +25,5 @@ set -e
 
 #sudo reboot
 
-sudo sed -i 's/^CONF_SWAPSIZE=[0-9]*$/CONF_SWAPSIZE=2048/' /etc/dphys-swapfile
+sudo sed -i 's/^CONF_SWAPSIZE=[0-9]*$/CONF_SWAPSIZE='${SWAPSIZE}'/' /etc/dphys-swapfile
 sudo /etc/init.d/dphys-swapfile restart
