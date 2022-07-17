618366443942614fd5b126941531b7d62be83ae1
wn1980 <waipotn@hotmail.com>
2022-07-17 19:49:40 +0700

    update

--- code-server-on-host.sh
+++ code-server-on-host.sh
@@ -14,14 +14,15 @@ then
     wget https://github.com/cdr/code-server/releases/download/v${VERSION}/code-server_${VERSION}_$(dpkg --print-architecture).deb && \
     sudo dpkg -i code-server_${VERSION}_$(dpkg --print-architecture).deb && \
     rm -f code-server_${VERSION}_$(dpkg --print-architecture).deb
+fi
 
 cat > "/etc/systemd/system/code-server.service" <<EOF
 [Unit]
 After=network.service
 [Service]
-User=computin
+User=ubuntu
 Group=sudo
-ExecStart=code-server --bind-addr 0.0.0.0:12345 --cert --auth none
+ExecStart=/usr/bin/code-server --bind-addr 0.0.0.0:12345 --cert --auth none
 Restart=on-failure
 RestartSec=5s
 [Install]
@@ -29,12 +30,12 @@ WantedBy=default.target
 
 EOF
 
-systemctl daemon-reload
+sudo systemctl daemon-reload
 
-systemctl enable code-server.service
+sudo systemctl enable code-server.service
 
-systemctl start code-server.service
+sudo systemctl start code-server.service
+
+sudo systemctl status code-server.service
 
 echo -e "\n===================\nInstall code-server complete...\n================="    
-                                                  
-fi   
