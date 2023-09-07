bce5a2bd976d721d0938270754927a51c934184a
wn1980 <waipotn@hotmail.com>
2023-09-08 06:06:00 +0700

    update

---
+++ github-ssh.sh
@@ -0,0 +1,19 @@
+#!/usr/bin/env bash
+
+set -e
+
+if [[ $# -ge 2 ]]
+    then 
+        echo "user.email $1, user.name $2."
+        git config --global user.email $1
+        git config --global user.name $2
+
+        echo "Generate new SSH key"
+        ssh-keygen -t ed25519 -C "$1"
+        ssh-keyscan -t ed25519 github.com >> ~/.ssh/known_hosts
+        cat ~/.ssh/ed25519.pub
+    else 
+        echo "Please provide github email and username ie.: ./github-ssh.sh me@web.com myname"
+fi
+
+ssh -T git@github.com
