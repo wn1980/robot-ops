9bac21ac75d238e94e5444811b9814b09bb49258
wn1980 <waipotn@hotmail.com>
2024-04-28 10:03:05 +0700

    update

--- nvidia-docker.sh
+++ nvidia-docker.sh
@@ -3,7 +3,8 @@
 set -e
 
 # Add the package repositories
-distribution=$(. /etc/os-release;echo $ID$VERSION_ID)
+# distribution=$(. /etc/os-release;echo $ID$VERSION_ID)
+distribution="ubuntu22.04" # hack for unsupported Ubuntu 24.04
 curl -s -L https://nvidia.github.io/nvidia-docker/gpgkey | sudo apt-key add -
 curl -s -L https://nvidia.github.io/nvidia-docker/$distribution/nvidia-docker.list | sudo tee /etc/apt/sources.list.d/nvidia-docker.list
 
